dfa749aebc70fed56d25920cda40b861dc0e4620
abdulhameed00815-ops <abdulhameed00815@gmail.com>
2026-03-01 23:53:45 +0200

    some print statement

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+int main(){
+	bool isFuckedUp = true;
+
+	// so, the line below means null (ik strange).
+	char grade = '\0';
+
+	if(isFuckedUp){
+		printf("you are fucked up!");
+	}
+}
