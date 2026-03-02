1cb231a02616b1e0fd1296162dd42c7470ac7207
abdulhameed00815-ops <abdulhameed00815@gmail.com>
2026-03-02 17:24:00 +0200

    that took way longer than it should 've

--- main.c
+++ main.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
+struct Server{
+	int port;
+	char name[50];
+};
 
 int main(){
-	bool isFuckedUp = true;
 
-	// so, the line below means null (ik strange).
-	char grade = '\0';
+	struct Server server1;
+	server1.port = 3000;
+	strcpy(server1.name, "lil server vert");
 
-	if(isFuckedUp){
-		printf("you are fucked up!");
-	}
-}
+	printf("%s", server1.name);
+
+	return 0;
+};
